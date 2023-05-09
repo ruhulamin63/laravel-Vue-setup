a5a95dc23307e17f14e2e7cdea3253e507edbd6b
ruhulamin63 <raridoy4@gmail.com>
2023-05-09 10:18:19 +0600

    update upload file

---
+++ resources/js/app.js
@@ -0,0 +1,14 @@
+// import './bootstrap';
+
+import { createApp } from 'vue';
+import Test from './components/Test.vue';
+
+// const app = createApp({
+//     components: {
+//         Test,
+//     },
+// });
+
+// app.mount('#app');
+
+createApp(Test).mount('#app');
---
+++ resources/js/components/Test.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <h1>Test</h1>
+    </div>
+</template>
+
+<script>
+    export default{
+        name: "Test",
+        data(){
+            return{
+                message: "Hello World"
+            }
+        }
+    }
+</script>
